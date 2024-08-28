bdbcd7c2d6ca233ef1f571911016e4937f1b45a1
Sunayana Das <135920492+sunayanadas21@users.noreply.github.com>
2024-08-28 19:52:55 +0000

    updated InsSortInt

--- InsSortInt.java
+++ InsSortInt.java
@@ -1,3 +1,20 @@
-public class InsSortInt {
+import java.io.File;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
 
+public class InsSortInt {
+    public static void main (String []args) throws FileNotFoundException{
+        String fileName= "randInts.txt";
+        File file= new File(fileName);
+        Scanner scan = new Scanner (file);
+        int []arr= new int [1000];
+        for (int i=0; i<arr.length; i++){
+            arr[i]= scan.nextInt();
+        }
+        System.out.println(arr[0]+ " "+ arr[999]);
+        scan.close();
+    }
+
+
+    
 }
